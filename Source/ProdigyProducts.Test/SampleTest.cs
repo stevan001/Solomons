e13a2d35f0263e38f953be3d0a8ea72ea3d36c8c
stevan001 <stevan.thomas@prodigysoft.net>
2012-09-02 10:55:07 -0400

    Upload to Git

---
+++ Source/ProdigyProducts.Test/SampleTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ProdigyProducts.Test
+{
+    [TestFixture]
+    public class SampleTest
+    {
+         [Test]
+        public void ShouldRunATest()
+         {
+             Assert.AreEqual(1,1+0);
+         }
+    }
+}
