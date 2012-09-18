224b1bbb9d0847ff7c806ee763d708f5935befa5
stevan001 <stevan.thomas@prodigysoft.net>
2012-09-18 16:26:18 -0400

    updates

--- Source/ProdigyProducts.Test/PresenterTests/ProductListingPresenterTest.cs
+++ Source/ProdigyProducts.Test/PresenterTests/ProductListingPresenterTest.cs
@@ -1,7 +1,32 @@
-﻿namespace ProdigyProducts.Test.PresenterTests
+﻿using NUnit.Framework;
+
+namespace ProdigyProducts.Test.PresenterTests
 {
+    [TestFixture]
     public class ProductListingPresenterTest
     {
-         
+        [Test]
+        public void ShouldGetListOfCategories()
+        {
+
+        }
+
+        [Test]
+        public void ShouldGetListOfProductsInACategory()
+        {
+
+        }
+
+        [Test]
+        public void ShouldSelectAProduct()
+        {
+
+        }
+
+        [Test]
+        public void ShouldRemoveAProduct()
+        {
+
+        }
     }
 }
