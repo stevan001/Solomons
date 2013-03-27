1c39e7303cc63c09931054199f8a23bb4dc4c804
stevan001 <stevan.thomas@prodigysoft.net>
2013-03-27 19:05:02 -0400

    testtest
    test

---
+++ Backup1/Source/Solomons.Web/contact.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ProdigyProducts.BLL.Services;
+
+namespace Solomons.Web
+{
+    public partial class contact : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("Name:{0} <br />", tbxName.Text);
+                sb.AppendLine();
+                sb.AppendFormat("Email:{0} <br />", tbxEmail.Text);
+                sb.AppendLine();
+                sb.AppendLine("Message:<br />");
+                sb.AppendLine(tbxMessage.Text);
+                EmailService.SendMail("stevan.thomas@hotmail.com","Solomons",tbxSubject.Text,sb.ToString());
+                lblStatuts.Text = "Your Message has been sent!";
+            }
+            catch (Exception exception)
+            {
+                lblStatuts.Text = "There was a problem sending your message. Error :"+ exception.Message; 
+
+            }
+        }
+    }
+}
